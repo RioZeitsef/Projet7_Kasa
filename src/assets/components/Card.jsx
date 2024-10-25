6cfed8ed1dc757538a7aa721a5db42a67f2b75d4
RioZeitsef <metz.matthieu@gmail.com>
2024-10-25 17:57:41 +0200

    Ajout des cartes et premiers essais de CSS

--- src/assets/components/Card.jsx
+++ src/assets/components/Card.jsx
@@ -2,11 +2,9 @@ import styles from "../style/Card.module.css";
 
 function Card({ cover, title }) {
   return (
-    <div className={styles.cardcontainer}>
-      <div className={styles.card}>
-        <img src={cover} />
-        <p className={styles.cardtitle}>{title}</p>
-      </div>
+    <div className={styles.card}>
+      <img className={styles.cardimg} src={cover} />
+      <p className={styles.cardtitle}>{title}</p>
     </div>
   );
 }
--- src/assets/components/Listelogements.jsx
+++ src/assets/components/Listelogements.jsx
@@ -1,9 +1,10 @@
 import logements from "./data/datalogements.json";
+import styles from "../style/Listelogements.module.css";
 import Card from "./Card.jsx";
 
 function ListeLogements() {
   return (
-    <div className="liste-logements">
+    <div className={styles.container}>
       {logements.map((logement, id) => (
         <Card key={id} cover={logement.cover} title={logement.title} />
       ))}
