9ca6ef93e000183bd4fcba67da5f29b26707d12a
Matthieu Metz <metz.matthieu@gmail.com>
2024-11-15 12:01:09 +0100

    finalisation de la page About, responsive OK

--- src/assets/components/Collapse.jsx
+++ src/assets/components/Collapse.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import styles from "../style/Collapse.module.css";
+import ArrowUp from "../images/arrow_up.svg";
 
 const Collapse = ({ title, children }) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -31,10 +32,22 @@ const Collapse = ({ title, children }) => {
 
   return (
     <div className={styles.collapse}>
-      <button className={styles.button} onClick={toggleMenu}>
-        {title}
+      <button className={styles.button}>
+        {title}{" "}
+        <img
+          onClick={toggleMenu}
+          src={ArrowUp}
+          alt="arrow"
+          className={`${styles.arrow} ${isOpen ? styles.rotate : ""}`}
+        />
       </button>
-      {isOpen && <div className={styles.collapsecontent}>{children}</div>}
+      {isOpen && (
+        <div
+          className={`${styles.collapsecontent} ${isOpen ? styles.show : ""}`}
+        >
+          {children}
+        </div>
+      )}
     </div>
   );
 };
