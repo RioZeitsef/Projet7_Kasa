c27828b073d9a7c24ef37024626e942443468497
Matthieu Metz <metz.matthieu@gmail.com>
2024-11-19 22:51:25 +0100

    mise en page des des pageslogements. reste à faire le reting (étoiles) et le responsive

--- src/assets/components/PageLogement.jsx
+++ src/assets/components/PageLogement.jsx
@@ -4,11 +4,14 @@ import Layout from "../components/Layout";
 import logements from "/src/data/datalogements.json";
 import ErrorPage from "../pages/ErrorPage.jsx";
 import Carousel from "../components/Carousel.jsx";
+import Collapse from "../components/Collapse.jsx";
 
 const PageLogement = () => {
   const { id } = useParams();
 
   const logement = logements.find((logement) => logement.id === id);
+
+  const tag = logements.find((tags) => tags.id === id).tags;
 
   if (!logement) {
     return <ErrorPage />;
@@ -16,25 +19,38 @@ const PageLogement = () => {
 
   return (
     <Layout>
-      <div>
-        <Carousel pictures={logement.pictures} />
-      </div>
-      <div className={styles.}>
+      <div className={styles.logementcontent}>
         <div>
+          <Carousel pictures={logement.pictures} />
+        </div>
+        <div className={styles.logementheader}>
           <h3 className={styles.title}>{logement.title}</h3>
-          <p className={styles.description}>{logement.description}</p>
           <p className={styles.location}>{logement.location}</p>
+          <div className={styles.tags}>
+            {tag.map((tags, index) => (
+              <span key={index} className={styles.tag}>
+                {tags}
+              </span>
+            ))}
+          </div>
         </div>
-        <div>
-          <span>{logement.tags}</span>
-        </div>
-        <div>
-          <div>
+        <div className={styles.ratingandname}>
+          {logement.rating}
+          <div className={styles.salerinfo}>
             <p>{logement.host.name}</p>
             <img src={logement.host.picture} />
           </div>
-          {logement.rating}
         </div>
+        <Collapse title="Description">
+          <p>{logement.description}</p>
+        </Collapse>
+        <Collapse title="Equipements">
+          {logement.equipments.map((equipment, index) => (
+            <li className={styles.liequipements} key={index}>
+              {equipment}
+            </li>
+          ))}
+        </Collapse>
       </div>
     </Layout>
   );
